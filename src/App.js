98bcdf3e01327f510091edb157513fb1088cafcc
Dorota Pindur <dpindur@wp.pl>
2021-03-17 20:24:03 +0100

    add cover image, create object currentSong

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ function Heading({ title }) {
   return <h1>{title}</h1>
 };
 
-function SongPlayer({ showControls = true, audioUrl }) {
+function SongPlayer({ showControls = true, song }) {
+  const { audioUrl, coverUrl } = song;
   return (
     <>
       <Heading title='Music Player'/>
+      <img width="250" height="250" src={coverUrl} alt="Song cover"/>
       <audio controls={showControls}>
         <source src={audioUrl} />
       </audio>
@@ -17,9 +19,17 @@ function SongPlayer({ showControls = true, audioUrl }) {
 
 
 function App() {
+  const currentSong = {
+    audioUrl: "https://examples.devmastery.pl/assets/audio/deadfro5h.mp3",
+    coverUrl: "https://examples.devmastery.pl/assets/audio/deadfro5h.jpg",
+    title: "Deadfro5h",
+    artist: "starfrosh"
+  };
   return (
     <div className="App">
-      <SongPlayer audioUrl="https://examples.devmastery.pl/assets/audio/deadfro5h.mp3"/>
+      <SongPlayer 
+        song = {currentSong}
+      />
     </div>
   );
 };
