e12cdc21ae5d64fce789cae595466d6b65a7d7a9
Dorota Pindur <dpindur@wp.pl>
2021-03-24 17:22:12 +0100

    add play and pause buttons, hide native audio controls

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function Heading({ title }) {
   return <h1>{title}</h1>
 };
 
-function SongPlayer({ showControls = true, song }) {
+function SongPlayer({ showControls = false, song }) {
+  const audioRef = useRef();
   const { audioUrl, coverUrl } = song;
   return (
     <>
       <Heading title='Music Player'/>
       <img width="250" height="250" src={coverUrl} alt="Song cover"/>
-      <audio key={audioUrl} controls={showControls}>
+      <audio ref={audioRef} key={audioUrl} controls={showControls}>
         <source src={audioUrl} />
       </audio>
+      <div>
+        <button onClick={() => audioRef.current.play()}>Play</button>
+        <button onClick={() => audioRef.current.pause()}>Pause</button>
+      </div>
     </>
   );
 };
